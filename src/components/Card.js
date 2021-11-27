e342fcaa5dd9b6f14792fabb72dfac32ea4fe377
Aaron Elam <44043565+5pez@users.noreply.github.com>
2021-11-26 23:43:00 -0600

    Update GitHub link

--- src/components/Card.js
+++ src/components/Card.js
@@ -35,7 +35,7 @@ export default function Card() {
                   </a>
                   <a
                     class="hover:cursor-pointer hover:text-indigo-400"
-                    href="https://www.github.com/AaronElam/"
+                    href="https://www.github.com/5pez/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
